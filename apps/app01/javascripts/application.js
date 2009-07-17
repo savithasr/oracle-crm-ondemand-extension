9dbf66cc59719e27582a4ca0291ea38265d4b5c0
srikanths <savitha.srikanth@gmail.com>
2009-07-17 10:16:32 -0400

    final

--- apps/app01/javascripts/application.js
+++ apps/app01/javascripts/application.js
@@ -1,4 +1,4 @@
-// sandbox ourselves to guarantee we don't interfere with OnDemand platform -savitha
+// sandbox ourselves to guarantee we don't interfere with OnDemand platform -savithasrikanth
 // JS internals
 jQuery(function($) {
     
